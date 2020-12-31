36d34478866ae390e3ea14e178da95adfbcfa363
Your Name <you@example.com>
2020-12-31 20:44:42 +0800

    add user by tom 20201231 from mhcommonentity-branch

--- MhCommon/src/main/java/com/meihong/entity/User.java
+++ MhCommon/src/main/java/com/meihong/entity/User.java
@@ -3,6 +3,10 @@ package com.meihong.entity;
 public class User {
     private String username;
     private String password;
+    private Integer age;
+    private boolean sex;
+    private Integer phone;
+    private String address;
 
     public String getUsername() {
         return username;
@@ -18,6 +22,38 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public boolean isSex() {
+        return sex;
+    }
+
+    public void setSex(boolean sex) {
+        this.sex = sex;
+    }
+
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 
 }
